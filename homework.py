0673d4c6c4b9edddaaaa4f3e3b73a52ca97aa88d
Виктория <peta-holding@yandex.ru>
2021-01-28 14:50:56 +0300

    Calculator 6

--- homework.py
+++ homework.py
@@ -28,6 +28,7 @@ class Record:
     def __init__(self, amount, comment, date=None):
         self.amount = amount
         self.comment = comment
+        
         if date:
             self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
         else:
@@ -39,19 +40,20 @@ class CashCalculator(Calculator):
     EURO_RATE = 91.74
     RUB_RATE = 1.0
 
-    currencies_code = {
-        'usd': ('USD', USD_RATE),
-        'eur': ('Euro', EURO_RATE),
-        'rub': ('руб', RUB_RATE)
-    }
-
     def get_today_cash_remained(self, currency_code):
-        if currency_code not in self.currencies_code:
-            return ValueError('Направильно указана валюта! Попробуйте еще раз.')
-        
-        limit_remains = self.limit - self.get_today_stats()
+        currencies_code = {
+            'usd': ('USD', self.USD_RATE),
+            'eur': ('Euro', self.EURO_RATE),
+            'rub': ('руб', self.RUB_RATE)
+        }
 
-        currency_code_name, currency_code_rate = self.currencies_code[currency_code]
+        if currency_code not in currencies_code:
+            return ValueError(
+                'Направильно указана валюта! Попробуйте еще раз.'
+                )
+
+        limit_remains = self.limit - self.get_today_stats()
+        currency_code_name, currency_code_rate = currencies_code[currency_code]
         remains_cash = round(limit_remains / currency_code_rate, 2)
 
         if limit_remains > 0:
@@ -67,36 +69,13 @@ class CashCalculator(Calculator):
 
 
 class CaloriesCalculator(Calculator):
-
     def get_calories_remained(self):
         used_today = self.get_today_stats()
         limit_remains = self.limit - used_today
+
         if limit_remains > 0:
             return (
                 f'Сегодня можно съесть что-нибудь ещё, но с общей '
                 f'калорийностью не более {limit_remains} кКал'
                 )
         return 'Хватит есть!'
-
-
-cash_calculator = CashCalculator(1000)
-cash_calculator.add_record(Record(amount=145, comment="кофе")) 
-cash_calculator.add_record(Record(amount=300, comment="Серёге за обед"))
-cash_calculator.add_record(Record(amount=3000, comment="бар в Танин др", \
-    date="11.11.2020"))
-                
-print(cash_calculator.get_today_cash_remained("rub"))
-print(cash_calculator.get_today_cash_remained("usd"))
-print(cash_calculator.get_today_cash_remained("eur"))
-
-calories_calculator = CaloriesCalculator(1500)
-calories_calculator.add_record(Record(amount=1186, comment="Кусок тортика. \
-    И ещё один."))
-calories_calculator.add_record(Record(amount=84, comment="Йогурт."))
-calories_calculator.add_record(Record(amount=1140, comment="Баночка чипсов.",\
-    date="24.02.2019"))
-calories_calculator.add_record(Record(amount=145, comment="кофе"))
-calories_calculator.add_record(Record(amount=145, comment="кофе"))
-
-print(calories_calculator.get_calories_remained())
-print(calories_calculator.get_week_stats(), 'кКал')
